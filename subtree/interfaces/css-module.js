8a50a2b69079859e559f4b931c8e4dff4349a629
svalencia <svalencia@greenflex.com>
2016-11-30 17:23:25 +0100

    clean

--- subtree/interfaces/css-module.js
+++
@@ -1,5 +0,0 @@
-// @flow
-// https://stackoverflow.com/questions/36912675/flow-required-module-not-found/37272883#37272883
-type CSSModule = { [key: string]: string }
-const emptyCSSModule: CSSModule = {}
-export default emptyCSSModule
